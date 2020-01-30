9ab1d2fc776fb13e2fafa1d70721ff23d9cda674
sloginov <s.loginov@sport-express.ru>
2020-01-30 17:59:52 +0300

    54 in process

--- app/Http/Controllers/DiggingDeeperController.php
+++ app/Http/Controllers/DiggingDeeperController.php
@@ -27,6 +27,34 @@ class DiggingDeeperController extends Controller
 //            $collection
 //        );
 
+//        $result['first'] = $collection->first();
+//        $result['last'] = $collection->last();
+       //dd($result);
+
+//        $result['where']['data'] = $collection
+//            ->where('category_id', 10)
+//            ->values()
+//            ->keyBy('id');
+//
+//
+//        $result['where']['count'] = $result['where']['data']->count();
+//        $result['where']['isEmpty'] = $result['where']['data']->isEmpty();
+//        $result['where']['isNotEmpty'] = $result['where']['data']->isNotEmpty();
+//        dd($result);
+
+//        $result['where_first'] = $collection
+//            ->firstWhere('created_at', '>', '2019-01-17 01:35:20');
+//        dd($result);
+
+        $result['map']['all'] = $collection->map(function (array $item) {
+            $newItem = new \stdClass();
+            $newItem->item_id = $item['id'];
+            $newItem->title_name = $item['title'];
+            $newItem->exists = is_null($item['deleted_at']);
+
+            return $newItem;
+        });
+        dd($result);
 
     }
 }
